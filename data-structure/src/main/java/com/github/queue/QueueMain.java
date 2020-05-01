0712cfa68a67c21705bb5306c562f15797caa79d
plum-wine <1078504296@qq.com>
2020-05-01 22:04:03 +0800

    有界阻塞队列

--- data-structure/src/main/java/com/github/queue/QueueMain.java
+++ data-structure/src/main/java/com/github/queue/QueueMain.java
@@ -16,8 +16,11 @@ public class QueueMain {
 
     private static final Logger LOGGER = LoggerFactory.getLogger(MethodHandles.lookup().lookupClass());
 
+    /**
+     * 测试put阻塞
+     */
     @Test
-    public void arrayBlockingQueue() throws InterruptedException {
+    public void arrayBlockingQueue1() throws InterruptedException {
         ArrayBlockingQueue<String> queue = new ArrayBlockingQueue<>(3);
         queue.put("a");
         queue.put("b");
@@ -33,7 +36,34 @@ public class QueueMain {
         }).start();
         LOGGER.info("开始插入第三个元素");
         queue.put("d");
-        LOGGER.info("end");
+        LOGGER.info("main end");
+    }
+
+    /**
+     * 测试take阻塞
+     */
+    @Test
+    public void arrayBlockingQueue2() throws InterruptedException {
+        ArrayBlockingQueue<String> queue = new ArrayBlockingQueue<>(3);
+        new Thread(() -> {
+            try {
+                for (int i = 0; i < 4; i++) {
+                    String data = queue.take();
+                    LOGGER.info("取出data:{}", data);
+                }
+            } catch (InterruptedException e) {
+                e.printStackTrace();
+            }
+        }).start();
+        Thread.sleep(1000);
+        queue.put("a");
+        Thread.sleep(1000);
+        queue.put("b");
+        Thread.sleep(1000);
+        queue.put("c");
+        Thread.sleep(1000);
+        queue.put("d");
+        LOGGER.info("main end");
     }
 
 }
