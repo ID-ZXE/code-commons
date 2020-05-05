ca0152f6db7923e06eaffe151f2def368c8ae78f
plum-wine <1078504296@qq.com>
2020-05-05 15:12:18 +0800

    dynamic

--- basic-api/src/main/java/com/github/thread/ThreadControlTools.java
+++ basic-api/src/main/java/com/github/thread/ThreadControlTools.java
@@ -1,6 +1,12 @@
 package com.github.thread;
 
 
+import org.junit.Test;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import java.lang.invoke.MethodHandles;
+
 /**
  * @author hangs.zhang
  * @date 19-7-24 下午8:51
@@ -9,7 +15,10 @@ package com.github.thread;
  */
 public class ThreadControlTools {
 
-    public static void main(String[] args) {
+    private static final Logger LOGGER = LoggerFactory.getLogger(MethodHandles.lookup().lookupClass());
+
+    @Test
+    public void test() {
         // 控制并发访问
         // 值可以动态修改
         // Semaphore
@@ -17,6 +26,15 @@ public class ThreadControlTools {
         // 值不可修改
         // one wait for all
         // CountDownLatch
+    }
+
+    @Test
+    public void test2() {
+        ThreadLocal<String> threadLocal = ThreadLocal.withInitial(() -> "initial");
+        threadLocal.set("value");
+        System.gc();
+        LOGGER.info("value:{}", threadLocal.get());
+        threadLocal.remove();
     }
 
 }
