0e0957d09864ddfa03fdb9bf4feeae1c8f6fd755
plum-wine <1078504296@qq.com>
2020-11-23 10:37:21 +0800

    python3 string

--- scripts/python-script/lang/_string.py
+++ scripts/python-script/lang/_string.py
@@ -1,23 +1,20 @@
-print("Hello World")
-
 message = "Hello World"
 print(message)
 # 读取字符串index位置的元素
 print(message[0])
-
+# 单引号, 双引号
 message2 = "'Hello World'"
 print(message2)
-
+# 换行
 print()
 # 首字母大写
 print(message.title())
 # 输出大写与小写
 print(message.upper())
 print(message.lower())
-
+# 字符串拼接
 first_name = "zhang"
 last_name = "hang"
-# 字符串拼接
 print(first_name + last_name)
 
 message3 = " python "
