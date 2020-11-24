2ba3b1841ebf0edba36e45a21fc14915309d32a0
plum-wine <1078504296@qq.com>
2020-11-25 00:56:21 +0800

    bash lang

--- scripts/shell-script/lang/commons.sh
+++ scripts/shell-script/lang/commons.sh
@@ -1,7 +1,6 @@
 # 获取命令结果
 # 单条命令
 # result=`command`
-
 # result=$(command)
 # 可以执行多条命令
 # result=$(pwd;ls)
@@ -25,9 +24,17 @@ calculation() {
 
 # 判定nginx进程是否存在
 protect_nginx() {
-  locale process_num=$(ps -ef | grep 'nginx' | grep -v 'grep' | wc -l)
+  local process_num=$(ps -ef | grep 'nginx' | grep -v 'grep' | wc -l)
   echo "${process_num} nginx is running"
 }
 
-echo "hello world"
-echo -e "hello \nworld"
+########################################
+# 通用技巧
+# 1. 获取当前脚本的目录
+base_dir=$(cd $(dirname $0); pwd)
+echo ${base_dir}
+# 2. 判断需要进入的目录是否存在, 不存在则退出
+cd /home || exit 1
+
+
+calculation
--- scripts/shell-script/lang/ssh.sh
+++ scripts/shell-script/lang/ssh.sh
@@ -1,3 +1,14 @@
 #!/usr/bin/env bash
-# 执行远程命令
-ssh root@server1 "cd /home; ls"
+# 单条命令
+ssh nick@xxx.xxx.xxx.xxx "ls"
+# 多条命令
+ssh nick@xxx.xxx.xxx.xxx "pwd; cat hello.txt"
+# 交互式命令
+ssh -t nick@xxx.xxx.xxx.xxx "top"
+# 远程执行本地脚本
+ssh nick@xxx.xxx.xxx.xxx <./test.sh
+# 远程执行本地脚本, 且传递参数
+ssh nick@xxx.xxx.xxx.xxx 'bash -s' hello <test.sh
+# 使用本地变量
+name=test
+ssh nick@xxx.xxx.xxx.xxx 'bash -c' "'echo $name'"
